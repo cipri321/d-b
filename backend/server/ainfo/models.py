bb306901fddfcd3c63f51cbaa1a5878a54ea3c82
marcrares <72352049+marcrares@users.noreply.github.com>
2022-03-31 23:08:09 +0300

    models
    de la raresi

--- backend/server/ainfo/models.py
+++ backend/server/ainfo/models.py
@@ -1,51 +1,75 @@
 from django.db import models
 
+
 # Create your models here.
 class Example(models.Model):
-    id=models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
+
+
+class Student_details(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+
+class Titles(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+
+class Teacher_details(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    title_id = models.ForeignKey(Titles, on_delete=models.CASCADE)
+
+
+class Faculty(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    chief_of_department_id = models.ForeignKey(Teacher_details, on_delete=models.CASCADE)
+
 
 class Year_of_study(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=40)
-    degree = models.CharField(max_length=40)
-    faculty_id = models.ForeignKey(Faculty, on_delete=models.Cascade)
+    name = models.CharField(max_length=50)
+    degree = models.CharField(max_length=50)
+    faculty_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
     enrolled = models.ManyToManyField(Student_details, through='Enrollment')
+
 
 class Course(models.Model):
     types = [
         ('O', 'Optional'),
         ('N', 'Not Optional'),
-        ]
+    ]
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=50)
     optional = models.CharField(max_length=1, choices=types)
     students = models.ManyToManyField(Student_details, through='Optional_preferences')
 
+
 class Curriculum(models.Model):
     id = models.AutoField(primary_key=True)
-    year_of_study_id = models.ForeignKey(Year_of_study, on_delete=models.Cascade)
+    year_of_study_id = models.ForeignKey(Year_of_study, on_delete=models.CASCADE)
     courses = models.ManyToManyField(Course, through='Curriculum_course')
+
 
 class Optional_preferences(models.Model):
     id = models.AutoField(primary_key=True)
     priority = models.IntegerField()
-    student_id = models.ForeignKey(Student_details, on_delete=models.Cascade)
-    course_id = models.ForeignKey(Course, on_delete=models.Cascade)
+    student_id = models.ForeignKey(Student_details, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+
 
 class Curriculum_course(models.Model):
     id = models.AutoField(primary_key=True)
-    curriculum_id = models.ForeignKey(Curriculum, on_delete=models.Cascade)
-    course_id = models.ForeignKey(Course, on_delete=models.Cascade)
+    curriculum_id = models.ForeignKey(Curriculum, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+
 
 class Enrollment(models.Model):
     id = models.AutoField(primary_key=True)
-    student_id = models.ForeignKey(Student_details, on_delete=models.Cascade)
-    year_of_study_id = models.ForeignKey(Year_of_study, on_delete=models.Cascade)
-
-
-
-
-
-
-
-
+    student_id = models.ForeignKey(Student_details, on_delete=models.CASCADE)
+    year_of_study_id = models.ForeignKey(Year_of_study, on_delete=models.CASCADE)
